86b9b9b0d2cc8cb276e93b4dfaf34fff1eee39cd
Matt Latham <latham75@gmail.com>
2016-10-04 15:13:42 -0500

    fixed boardState and stopped overwrite

--- public/main.js
+++ public/main.js
@@ -5,19 +5,47 @@ const socket = io()
 socket.on('connect', () => console.log(`Socket connected: ${socket.id}`))
 socket.on('disconnect', () => console.log(`Socket connected`))
 
-const board = [
-  [,,],
-  [,,],
-  [,,],
+const boardState = [
+  ['','',''],
+  ['','',''],
+  ['','',''],
 ]
 
-const table = document.querySelector('table')
+const drawBoard = (boardState) => {
+  document.querySelector('.board').innerHTML = `
+  <table>
+    <tr>
+      <td>${boardState[0][0]}</td>
+      <td>${boardState[0][1]}</td>
+      <td>${boardState[0][2]}</td>
+    </tr>
+    <tr>
+      <td>${boardState[1][0]}</td>
+      <td>${boardState[1][1]}</td>
+      <td>${boardState[1][2]}</td>
+    </tr>
+    <tr>
+      <td>${boardState[2][0]}</td>
+      <td>${boardState[2][1]}</td>
+      <td>${boardState[2][2]}</td>
+    </tr>
+  </table
+  `
+}
+drawBoard(boardState)
+let nextPlayer = "O"
+const board = document.querySelector('.board')
 
-table.addEventListener('click', event => {
+board.addEventListener('click', event => {
   const column = event.target.cellIndex
-  const row = event.target.parentElement.rowIndex
-  board[row][column] = "O"
-  event.target.innerText = "X"
+  const row = event.target.closest('tr').rowIndex
+  if (boardState[row][column]){
+    return console.log("Cannot move there")
+  }
+  boardState[row][column] = nextPlayer
+  nextPlayer = nextPlayer === "X" ? "O" : "X"
+  drawBoard(boardState)
+  // event.target.innerText = "X"
   console.log(`Current game state:`, board)
 
 })
