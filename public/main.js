8e1e61531dc76c6dab7e6dbbec4e3ab38753c868
Matt Latham <latham75@gmail.com>
2016-10-04 14:58:56 -0500

    can add X's and create a board state

--- public/main.js
+++ public/main.js
@@ -5,7 +5,19 @@ const socket = io()
 socket.on('connect', () => console.log(`Socket connected: ${socket.id}`))
 socket.on('disconnect', () => console.log(`Socket connected`))
 
+const board = [
+  [,,],
+  [,,],
+  [,,],
+]
+
 const table = document.querySelector('table')
+
 table.addEventListener('click', event => {
-  console.log(event.target)
+  const column = event.target.cellIndex
+  const row = event.target.parentElement.rowIndex
+  board[row][column] = "O"
+  event.target.innerText = "X"
+  console.log(`Current game state:`, board)
+
 })
