7c6cf013447d6fee0cd12cba75d85b072817725e
Matt Latham <latham75@gmail.com>
2016-10-04 15:29:46 -0500

    updated for game move eval

--- public/main.js
+++ public/main.js
@@ -1,9 +1,12 @@
-'use strict';
+'use strict'
 
 const socket = io()
 
 socket.on('connect', () => console.log(`Socket connected: ${socket.id}`))
-socket.on('disconnect', () => console.log(`Socket connected`))
+socket.on('disconnect', () => console.log('Socket disconnected'))
+
+const board = document.querySelector('.board')
+const status = document.querySelector('.status')
 
 const boardState = [
   ['','',''],
@@ -11,41 +14,97 @@ const boardState = [
   ['','',''],
 ]
 
-const drawBoard = (boardState) => {
-  document.querySelector('.board').innerHTML = `
-  <table>
-    <tr>
-      <td>${boardState[0][0]}</td>
-      <td>${boardState[0][1]}</td>
-      <td>${boardState[0][2]}</td>
-    </tr>
-    <tr>
-      <td>${boardState[1][0]}</td>
-      <td>${boardState[1][1]}</td>
-      <td>${boardState[1][2]}</td>
-    </tr>
-    <tr>
-      <td>${boardState[2][0]}</td>
-      <td>${boardState[2][1]}</td>
-      <td>${boardState[2][2]}</td>
-    </tr>
-  </table
+let nextPlayer = 'X'
+
+const drawBoard = b => {
+  board.innerHTML = `
+    <table>
+      <tr>
+        <td>${b[0][0]}</td>
+        <td>${b[0][1]}</td>
+        <td>${b[0][2]}</td>
+      </tr>
+      <tr>
+        <td>${b[1][0]}</td>
+        <td>${b[1][1]}</td>
+        <td>${b[1][2]}</td>
+      </tr>
+      <tr>
+        <td>${b[2][0]}</td>
+        <td>${b[2][1]}</td>
+        <td>${b[2][2]}</td>
+      </tr>
+    </table>
   `
+
+  status.innerText = `${nextPlayer}'s Turn`
 }
-drawBoard(boardState)
-let nextPlayer = "O"
-const board = document.querySelector('.board')
 
-board.addEventListener('click', event => {
-  const column = event.target.cellIndex
-  const row = event.target.closest('tr').rowIndex
-  if (boardState[row][column]){
-    return console.log("Cannot move there")
+const winner = b => {
+  // Rows
+  if (b[0][0] && b[0][0] === b[0][1] && b[0][1] === b[0][2]) {
+    return b[0][0]
   }
-  boardState[row][column] = nextPlayer
-  nextPlayer = nextPlayer === "X" ? "O" : "X"
-  drawBoard(boardState)
-  // event.target.innerText = "X"
-  console.log(`Current game state:`, board)
 
+  if (b[1][0] && b[1][0] === b[1][1] && b[1][1] === b[1][2]) {
+    return b[1][0]
+  }
+
+  if (b[2][0] && b[2][0] === b[2][1] && b[2][1] === b[2][2]) {
+    return b[2][0]
+  }
+
+  // Cols
+  if (b[0][0] && b[0][0] === b[1][0] && b[1][0] === b[2][0]) {
+    return b[0][0]
+  }
+
+  if (b[0][1] && b[0][1] === b[1][1] && b[1][1] === b[2][1]) {
+    return b[0][1]
+  }
+
+  if (b[0][2] && b[0][2] === b[1][2] && b[1][2] === b[2][2]) {
+    return b[0][2]
+  }
+
+  // Diags
+  if (b[0][0] && b[0][0] === b[1][1] && b[1][1] === b[2][2]) {
+    return b[0][0]
+  }
+
+  if (b[0][2] && b[0][2] === b[1][1] && b[1][1] === b[2][0]) {
+    return b[0][2]
+  }
+
+  // Tie or In-Progress
+  else {
+    return null
+  }
+}
+
+drawBoard(boardState)
+
+board.addEventListener('click', evt => {
+  const col = evt.target.cellIndex
+  const row = evt.target.closest('tr').rowIndex
+
+  if (boardState[row][col]) {
+    return console.log('Cannot move there')
+  }
+
+  if (winner(boardState)) {
+    return console.log('Game is over!')
+  }
+
+  boardState[row][col] = nextPlayer
+  drawBoard(boardState)
+  console.log('Current game state:', board)
+
+  if (winner(boardState)) {
+    return status.innerText = `${nextPlayer} WON!`
+  }
+
+
+  nextPlayer = nextPlayer === 'X' ? 'O' : 'X'
+  status.innerText = `${nextPlayer}'s Turn`
 })
