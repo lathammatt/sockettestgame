d701926ed73fae37bc9e990719d016abccb3e9df
Matt Latham <latham75@gmail.com>
2016-10-05 11:04:13 -0500

    finished moving game logic to backend

--- server.js
+++ server.js
@@ -32,6 +32,7 @@ const Game = mongoose.model('game', {
     [String, String, String],
   ],
   nextMove: String,
+  result: String,
 })
 
 io.on('connect', socket => {
@@ -53,12 +54,68 @@ io.on('connect', socket => {
     })
 
   socket.on('make move', move => {
-    socket.game.board[row][col] = socket.game.nextMove
+    if (socket.game.result){
+      return
+    }
+
+    if (socket.game.board[row][col]){
+      return
+    }
+
+    socket.game.board[row][col] = socket.game.toMove
     socket.game.toMove = socket.game.toMove === 'X' ? 'O' : 'X'
     socket.game.markModified('board') //will let mongoose know that the board property has been modified because mongoose still thinks each game state change is the same array and will update with blanks
+    const result = winner(socket.game.board)
+
+    if (winner(game.board)) {
+      socket.game.toMove = undefined // how to delete a property in mongoose
+      socket.game.result = result
+    }
     socket.game.save().then(g =>
     socket.emit('move made', g))
   })
   console.log(`Socket connected: ${socket.id}`)
   socket.on('disconnect', () => console.log(`Socket disconnected: ${socket.id}`))
 })
+
+const winner = b => {
+  // Rows
+  if (b[0][0] && b[0][0] === b[0][1] && b[0][1] === b[0][2]) {
+    return b[0][0]
+  }
+
+  if (b[1][0] && b[1][0] === b[1][1] && b[1][1] === b[1][2]) {
+    return b[1][0]
+  }
+
+  if (b[2][0] && b[2][0] === b[2][1] && b[2][1] === b[2][2]) {
+    return b[2][0]
+  }
+
+  // Cols
+  if (b[0][0] && b[0][0] === b[1][0] && b[1][0] === b[2][0]) {
+    return b[0][0]
+  }
+
+  if (b[0][1] && b[0][1] === b[1][1] && b[1][1] === b[2][1]) {
+    return b[0][1]
+  }
+
+  if (b[0][2] && b[0][2] === b[1][2] && b[1][2] === b[2][2]) {
+    return b[0][2]
+  }
+
+  // Diags
+  if (b[0][0] && b[0][0] === b[1][1] && b[1][1] === b[2][2]) {
+    return b[0][0]
+  }
+
+  if (b[0][2] && b[0][2] === b[1][1] && b[1][1] === b[2][0]) {
+    return b[0][2]
+  }
+
+  // Tie or In-Progress
+  else {
+    return null
+  }
+}
